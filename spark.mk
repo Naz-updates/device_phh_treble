1957875ea4f988eda3975591b26d67e30f37aa03
naz664 <nazimnavas145@gmail.com>
2022-12-22 14:03:46 +0000

    include some extra flags -> spark.mk

--- spark.mk
+++ spark.mk
@@ -15,9 +15,19 @@ TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
+# Use CCache
+USE_CCACHE := true
+
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/SparkOS_gsi/13/ota.json
+	persist.sys.binary_xml=false \
+	dalvik.vm.dex2oat64.enabled=true \
 
 #prebuilts
 PRODUCT_PACKAGES += \
   OpenEUICC \
+
+# Set Bootanimation at 720P
+TARGET_BOOT_ANIMATION_RES := 720
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
